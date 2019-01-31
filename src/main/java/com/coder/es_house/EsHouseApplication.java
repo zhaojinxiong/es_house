fdbc5453d0e606f1ba6965c7b21c7ea3da8d8209
zhaojx <376053926@qq.com>
2019-01-31 19:30:46 +0800

    Initial commit

---
+++ src/main/java/com/coder/es_house/EsHouseApplication.java
@@ -0,0 +1,14 @@
+package com.coder.es_house;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsHouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsHouseApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/coder/es_house/EsHouseApplicationTests.java
@@ -0,0 +1,17 @@
+package com.coder.es_house;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EsHouseApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
