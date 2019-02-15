6ede4b7138b3803ff718de12ee6729490989ade4
zhaojx <376053926@qq.com>
2019-02-15 21:32:28 +0800

    项目初始化

--- src/main/java/com/coder/es_house/web/controller/admin/AdminController.java
+++ src/main/java/com/coder/es_house/web/controller/admin/AdminController.java
@@ -1,7 +1,17 @@
 package com.coder.es_house.web.controller.admin;
 
+import com.coder.es_house.base.ApiResponse;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Method;
 
 /**
  * @author:apple
@@ -30,6 +40,29 @@ public class AdminController {
     public String logoutPage(){
 
         return  "logout";
+    }
+
+    @GetMapping("admin/add/house")
+    public String addHousePage(){
+        return "admin/house-add";
+    }
+
+    @PostMapping(value = "admin/upload/photo", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @ResponseBody
+    public ApiResponse uploadPhoto(@RequestParam("file")MultipartFile file){
+        if( file.isEmpty()){
+            return ApiResponse.ofStatus(ApiResponse.Status.NOT_VALID_PARAM);
+        }
+        String fileName = file.getOriginalFilename();
+        File target = new File("/Users/apple/java/es_house/tmp"+fileName);
+        try {
+            file.transferTo(target);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return ApiResponse.ofStatus(ApiResponse.Status.INTERNAL_SERVER_ERROR);
+        }
+        return ApiResponse.ofSuccess(null);
+
     }
 
 }
